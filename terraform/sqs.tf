69ddf53b2987825987095e205259d4669b5d5e18
Thaeli <thaeli@magfest.org>
2019-07-09 23:00:48 -0400

    Grant permission on analysis queue for all buckets listed in external_s3_bucket_resources

--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -25,16 +25,16 @@ data "aws_iam_policy_document" "analyzer_queue_policy" {
     actions   = ["sqs:SendMessage"]
     resources = ["${aws_sqs_queue.analyzer_queue.arn}"]
 
-    // Allow only the BinaryAlert S3 bucket to notify the SQS queue.
+    // Allow the BinaryAlert bucket and any configured external_s3_buckets to notify the SQS queue.
     condition {
       test     = "ArnEquals"
       variable = "aws:SourceArn"
-      values   = ["${aws_s3_bucket.binaryalert_binaries.arn}"]
+      values = ["${concat(list(aws_s3_bucket.binaryalert_binaries.arn), var.external_s3_bucket_resources)}"]
     }
   }
 }
 
-// Allow analyzer queue to be sent messages from the BinaryAlert S3 bucket.
+// Allow analyzer queue to be sent messages from the BinaryAlert S3 bucket and other configured external_s3_buckets.
 resource "aws_sqs_queue_policy" "analyzer_queue_policy" {
   queue_url = "${aws_sqs_queue.analyzer_queue.id}"
   policy    = "${data.aws_iam_policy_document.analyzer_queue_policy.json}"
